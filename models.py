06cdc62a704c65e008cf4464b3c6ee5dd6f182a8
swmo <info@swmo.ch>
2019-01-03 18:53:02 +0000

    refactor: pycodestyle for models

--- models.py
+++ models.py
@@ -8,66 +8,107 @@ BaseDb = declarative_base()
 
 
 class User(BaseDb):
-  __tablename__ = 'user'
+    __tablename__ = 'user'
 
-  id = Column(Integer, primary_key = True)
-  email = Column(String(250),nullable=False,unique=True)
-  name = Column(String(80))
-  picture = Column(String(250),default='/static/images/default_avatar.png')
-  password = Column(String(250),nullable=True)
+    id = Column(
+        Integer,
+        primary_key=True
+        )
+    email = Column(
+        String(250),
+        nullable=False,
+        unique=True
+        )
+    name = Column(
+        String(80)
+        )
+    picture = Column(
+        String(250),
+        default='/static/images/default_avatar.png'
+        )
+    password = Column(
+        String(250),
+        nullable=True
+        )
+    items = relationship(
+        'CatalogItem',
+        backref='user',
+        lazy=True
+        )
 
-  items = relationship('CatalogItem', backref='user', lazy=True)
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
 
-
-  @property
-  def serialize(self):
-     return {
-         'id'           : self.id,
-         'name'         : self.name
-     }
-
-  
 
 class CatalogCategory(BaseDb):
-    __tablename__ = 'catalog_category'
+        __tablename__ = 'catalog_category'
 
-    id = Column(Integer, primary_key = True)
-    name =Column(String(80), nullable = False)
-    background = Column(String(250),default='default.jpg')
-    items = relationship('CatalogItem', backref='category', lazy=True)
+        id = Column(
+            Integer,
+            primary_key=True
+            )
+        name = Column(
+            String(80),
+            nullable=False
+            )
+        background = Column(
+            String(250),
+            default='default.jpg'
+            )
+        items = relationship(
+            'CatalogItem',
+            backref='category',
+            lazy=True
+            )
 
+        @property
+        def serialize(self):
+            items = ([i.serialize for i in self.items])
+            return {
+                'id': self.id,
+                'name': self.name,
+                'items': items
+            }
 
-    @property
-    def serialize(self):
-       """Return object data in easily serializeable format"""
-       items = ([i.serialize for i in self.items])
-       return {
-           'id'         : self.id,
-           'name'       : self.name,
-           'items'      : items
-       }
-       #json.dumps([(dict(row.items())) for row in rs])
 
 class CatalogItem(BaseDb):
-    __tablename__ = 'catalog_item'
+        __tablename__ = 'catalog_item'
 
-    id = Column(Integer, primary_key = True)
-    name =Column(String(80), nullable = False)
-    description = Column(String(1000))
-    user_id = Column(Integer,ForeignKey('user.id'))
-    category_id = Column(Integer,ForeignKey('catalog_category.id'))
+        id = Column(
+            Integer,
+            primary_key=True
+            )
+        name = Column(
+            String(80),
+            nullable=False
+            )
+        description = Column(
+            String(1000)
+            )
+        user_id = Column(
+            Integer,
+            ForeignKey('user.id')
+            )
+        category_id = Column(
+            Integer,
+            ForeignKey('catalog_category.id')
+            )
 
+        @property
+        def serialize(self):
+            user = ([self.user.serialize])
+            return {
+                'id': self.id,
+                'name': self.name,
+                'description': self.description,
+                'created_by': user
+            }
 
-    @property
-    def serialize(self):
-      user = ([self.user.serialize])
-      return {
-           'id'           : self.id,
-           'name'         : self.name,
-           'description'  : self.description,
-           'created_by'       : user
-       }
 
 engine = create_engine('sqlite:///catalog.db')
- 
+
 BaseDb.metadata.create_all(engine)
